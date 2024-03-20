718de3b08e87a074accc3d2745a5e68bb935ff29
=sibel97 <s_hassan97@hotmail.co.uk>
2024-03-20 10:52:13 +0000

    Write

--- Tech246CodeAlong/src/main/java/com/sparta/shh/readwrite/ReadWriteApp.java
+++ Tech246CodeAlong/src/main/java/com/sparta/shh/readwrite/ReadWriteApp.java
@@ -6,6 +6,7 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.ArrayList;
 import java.util.List;
 
 public class ReadWriteApp
@@ -13,6 +14,7 @@ public class ReadWriteApp
     public static void main(String[] args) {
         String myPathString = "src/main/resources/Todo.txt";
         String myOutputPathString = "src/main/resources/output.txt";
+        String myOutputPathString2 = "src/main/resources/listOutput2.txt";
         System.out.println("Read all lines using nio");
 
         var result = readNioAllLines(myPathString);
@@ -26,7 +28,23 @@ public class ReadWriteApp
         System.out.println("write using nio - all text");
         String whatToWrite = "Pretend this is a very long string that I want to write to a file";
         writeNio(myOutputPathString,whatToWrite);
+        List<String> fruits = new ArrayList<>();
+        fruits.add("Apple");
+        fruits.add("Banana");
+        fruits.add("Cherry");
+        writeStringListNio(myOutputPathString2,result);
 
+    }
+
+    private static void writeStringListNio(String pathString, List<String> result)
+    {
+        Path thePath = Paths.get(pathString);
+        try{
+            Files.write(thePath,result.stream().toList());
+        }catch (IOException e)
+        {
+            e.printStackTrace();
+        }
     }
 
     private static void writeNio(String pathString, String whatToWrite)
